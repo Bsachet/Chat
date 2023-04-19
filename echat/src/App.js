f137b2c2a14a9341be81af9316a4f5af4a57eb21
Bsachet <88344845+Bsachet@users.noreply.github.com>
2023-04-19 19:57:43 -0300

    Add files via upload

--- echat/src/App.js
+++ echat/src/App.js
@@ -2,13 +2,15 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import "./styles.scss"
 import { useContext } from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import {AuthContext} from "./context/AuthContext";
+
 
 function App() {
+
+const {currentUser} = useContext(AuthContext)
+console.log(currentUser)
+
   return (
     <BrowserRouter>
       <Routes>
