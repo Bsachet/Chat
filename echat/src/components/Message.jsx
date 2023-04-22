0348874b42464f6447804ca83fe9e8d12e7d48fc
Bsachet <88344845+Bsachet@users.noreply.github.com>
2023-04-22 12:09:46 -0300

    Add files via upload

--- echat/src/components/Message.jsx
+++ echat/src/components/Message.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-const Message = () => {
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+
   return (
-    <div className='message owner'>
+    <div
+      ref={ref}
+      className={`message ${message.senderId === currentUser.uid && "owner"}`}
+    >
       <div className="messageInfo">
-        <img src="https://i.pinimg.com/564x/49/ce/d2/49ced2e29b6d4945a13be722bac54642.jpg" alt=""/>
-        <span>Agora</span>
+        <img
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
+          alt=""
+        />
+        <span>agora</span>
       </div>
       <div className="messageContent">
-        <p>Olá.</p>
-        <img src="" alt=""/>
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
--- echat/src/components/Navbar.jsx
+++ echat/src/components/Navbar.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {signOut} from "firebase/auth"
 import {auth} from "../firebase"
+import {AuthContext} from '../context/AuthContext'
 
 const Navbar = () => {
+  const {currentUser} = useContext(AuthContext)
+  
   return (
     <div className='navbar'>
       <span className='logo'>&Chat</span>
       <div className='user'>
-        <img src="https://i.pinimg.com/564x/49/ce/d2/49ced2e29b6d4945a13be722bac54642.jpg" alt="" />
-        <span>Nome</span>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.name}</span>
         <button onClick={()=>signOut(auth)}>Sair</button>
       </div>
     </div>
